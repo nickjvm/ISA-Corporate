8341bbe85898bfee35708cacef740743235d03be
Nick VanMeter <nickjvm@gmail.com>
2013-12-07 09:18:12 -0800

    Custom market module
    Contains default block and customizable full page view.

--- sites/all/themes/harvest/templates/block.tpl.php
+++ sites/all/themes/harvest/templates/block.tpl.php
@@ -52,6 +52,8 @@
   <h4<?php print $title_attributes; ?>><?php print $block->subject ?></h4>
   <?php } elseif ($block->delta == "weather") { ?>
   <h3<?php print $title_attributes; ?>><?php print $block->subject ?></h3>
+   <?php } elseif ($block->delta == "marketsnapshot") { ?>
+  <h3<?php print $title_attributes; ?>><?php print $block->subject ?></h3>
 	<?php } else { ?>
   <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
   <?php } ?>
